58f5d15765fe57d135c767d3c2c318862f3a0c55
Saige Cross <saigecrossdev@gmail.com>
2022-06-06 20:46:15 -0500

    adding routes to goalRoutes.js

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'YAHOOO!!' })
+})
+
+router.post('/', (req, res) => {
+  res.status(200).json({ message: 'YAHOOO!!' })
+})
+
+router.put('/', (req, res) => {
+  res.status(200).json({ message: 'YAHOOO!!' })
+})
+
+router.delete('/', (req, res) => {
+  res.status(200).json({ message: 'YAHOOO!!' })
+})
+
+module.exports = router
