8115a2666504d0e3a02cd4c86a550d9bf2f71131
Saige Cross <saigecrossdev@gmail.com>
2022-06-07 16:21:38 -0500

    changed routes name

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.status(200).json({ message: 'YAHOOO!!' })
+  res.status(200).json({ message: 'check, check, check.' })
 })
 
 router.post('/', (req, res) => {
