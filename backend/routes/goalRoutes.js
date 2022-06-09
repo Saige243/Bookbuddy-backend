2b3cbe04a5e798587a5dccb4ccd35006a50423be
Saige Cross <saigecrossdev@gmail.com>
2022-06-09 08:06:48 -0500

    updated file names - now figure out why storing MDB in 'test' collection

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controller/controller')
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controller/goalController')
 
 router.route('/').get(getGoals).post(setGoal)
 router.route('/:id').put(updateGoal).delete(deleteGoal)
