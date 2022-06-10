9ba5042605d9e0f72bb3f4140c6f671081116ac0
Saige Cross <saigecrossdev@gmail.com>
2022-06-09 20:35:48 -0500

    added colors to app.listen console log

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,4 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}.`.cyan.underline))
