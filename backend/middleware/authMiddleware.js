ebbc376dcc32ccd68c287cfc43be32837d1891c1
Saige Cross <saigecrossdev@gmail.com>
2022-06-10 20:57:13 -0500

    private route generated from JWT token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,16 +5,18 @@ const User = require('../models/userModel')
 const protect = asyncHandler(async (req, res, next) => {
   let token
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')) {
     try {
       // get token from header
-      token = req.header.authorization.split(' ')[1]
+      token = req.headers.authorization.split(' ')[1]
 
       // verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
       // get user from the token
       req.user = await User.findById(decoded.id).select('-password')
+
       next()
     } catch (error) {
       console.log(error)
