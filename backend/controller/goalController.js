c5f9c8eb4c001dc341aa88d6f68cf08ca6255bf6
Saige Cross <saigecrossdev@gmail.com>
2022-06-12 14:57:33 -0500

    CRUD capability completely finished with authorization and authentication

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+// Not sure what User is used for...
 const User = require('../models/userModel')
 
 // @desc Get Goals
