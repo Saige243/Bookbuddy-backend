e1fcce55ce5a79989e6ce45e9c99226d29d50a3f
Saige Cross <saigecrossdev@gmail.com>
2022-06-18 21:18:52 -0500

    working on register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,30 @@
-
+import { useState, useEffect } from 'react'
+import { FaUser } from 'react-icons/fa'
 
 function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: '',
+    password2: ''
+  })
+
+  const { name, email, password, password2 } = formData
+
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <section>
+        <h1>
+          <FaUser /> Register
+      </h1>
+        <p>Please create an account</p>
+      </section>
+      <section>
+        <form>
+          <input type="text" className="form-control" id="name" name="name" value={name} />
+        </form>
+      </section>
+    </>
   )
 }
 
