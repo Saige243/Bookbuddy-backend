e0302942da162742808fb3f52c1047c029d83934
Saige Cross <saigecrossdev@gmail.com>
2022-07-04 13:57:52 -0500

    delete goals working

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,4 +1,8 @@
+import { useDispatch } from 'react-redux'
+import { deleteGoal } from '../features/goals/goalSlice'
+
 function GoalItem({ goal }) {
+  const dispatch = useDispatch()
   return (
     <div>
       <div className="goal">
@@ -6,6 +10,7 @@ function GoalItem({ goal }) {
           {new Date(goal.createdAt).toLocaleString('en-US')}
         </div>
         <h2>{goal.text}</h2>
+        <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">X</button>
       </div>
     </div>
   )
